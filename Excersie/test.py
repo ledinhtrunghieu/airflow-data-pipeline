eec3515ce10a1cb4b190ccb58a99db18211fc99b
ledinhtrunghieu <ledinhtrunghieuvt99@gmail.com>
2021-06-22 13:34:33 +0700

    Create test.py

---
+++ Excersie/test.py
@@ -0,0 +1,467 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.hooks.S3_hook import S3Hook
+
+AWS_KEY = os.environ.get('AWS_KEY')
+AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args= {
+    'owner': 'udacity',
+    'start_date': datetime.now(),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_retry': False
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='@hourly',
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
+
+create_all_tables = PostgresOperator(
+    task_id="create_all_tables",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=SqlQueries.create_all_tables
+)
+
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="dend",
+    s3_key="log_data"
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    table="staging_songs",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="dend",
+    s3_key="song_data"
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql=SqlQueries.songplay_table_insert
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql=SqlQueries.user_table_insert
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql=SqlQueries.song_table_insert
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql=SqlQueries.artist_table_insert
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    sql=SqlQueries.time_table_insert
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    check_stmts=[
+        {
+            'sql': 'SELECT COUNT(*) FROM songplays;',
+            'op': 'gt',
+            'val': 0
+        },
+        {
+            'sql': 'SELECT COUNT(*) FROM songplays WHERE songid IS NULL;',
+            'op': 'eq',
+            'val': 0
+        }
+    ]
+)
+end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
+
+
+# Setup Task Dependacies
+
+# Step 1
+start_operator >> create_all_tables 
+
+# Step 2
+create_all_tables >> stage_events_to_redshift
+create_all_tables >> stage_songs_to_redshift
+
+# Step 3
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+# Step 4
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
+
+
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+    
+    template_fields = ("s3_keys",)
+    
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        DELIMITER '{}'
+    """
+    
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 delimiter=",",
+                 ignore_headers=1,
+                 *args, **kwargs):
+
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
+
+    def execute(self, context):
+        aws_hook = AwsHook("aws_credentials")
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+
+        self.log.info("Copying data from S3 to Redshift")
+        rendered_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+            self.table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            self.ignore_headers,
+            self.delimiter
+        )
+        redshift.run(formatted_sql)
+
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+
+    ui_color = '#F98866'
+    
+    copy_sql = """
+        {}
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        DELIMITER '{}'
+    """
+    
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 sql = "",
+                 delimiter=",",
+                 ignore_headers=1,
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
+        self.sql = sql
+
+    def execute(self, context):
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Inserting data to Redshift")
+        formatted_sql = LoadFactOperator.copy_sql.format(
+            self.sql,
+            credentials.access_key,
+            credentials.secret_key,
+            self.ignore_headers,
+            self.delimiter
+        )
+        redshift.run(formatted_sql)
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+    copy_sql = """
+        {}
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        DELIMITER '{}'
+    """
+    
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 sql= "",
+                 delimiter=",",
+                 ignore_headers=1,
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
+        self.sql= sql
+        
+    def execute(self, context):
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Inserting data to Redshift")
+        formatted_sql = LoadDimensionOperator.copy_sql.format(
+            self.sql,
+            credentials.access_key,
+            credentials.secret_key,
+            self.ignore_headers,
+            self.delimiter
+        )
+        redshift.run(formatted_sql)
+
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class DataQualityOperator(BaseOperator):
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id='redshift',
+                 check_stmts=[],
+                 *args, **kwargs):
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.check_stmts = check_stmts
+
+    def execute(self, context):
+        redshift_hook = PostgresHook("redshift")
+
+        for stmt in self.check_stmts:
+            result = int(redshift_hook.get_first(sql=stmt['sql'])[0])
+
+            # check if equal
+            if stmt['op'] == 'eq':
+                if result != stmt['val']:
+                    raise AssertionError(f"Check failed: {result} {stmt['op']} {stmt['val']}")
+            # check if not equal
+            elif stmt['op'] == 'ne':
+                if result == stmt['val']:
+                    raise AssertionError(f"Check failed: {result} {stmt['op']} {stmt['val']}")
+            # check if greater than
+            elif stmt['op'] == 'gt':
+                if result <= stmt['val']:
+                    raise AssertionError(f"Check failed: {result} {stmt['op']} {stmt['val']}")
+
+            self.log.info(f"Passed check: {result} {stmt['op']} {stmt['val']}")
+
+
+class SqlQueries:
+    create_all_tables = ("""
+        CREATE TABLE public.artists (
+        artistid varchar(256) NOT NULL,
+        name varchar(256),
+        location varchar(256),
+        lattitude numeric(18,0),
+        longitude numeric(18,0)
+    );
+
+    CREATE TABLE public.songplays (
+        playid varchar(32) NOT NULL,
+        start_time timestamp NOT NULL,
+        userid int4 NOT NULL,
+        "level" varchar(256),
+        songid varchar(256),
+        artistid varchar(256),
+        sessionid int4,
+        location varchar(256),
+        user_agent varchar(256),
+        CONSTRAINT songplays_pkey PRIMARY KEY (playid)
+    );
+
+    CREATE TABLE public.songs (
+        songid varchar(256) NOT NULL,
+        title varchar(256),
+        artistid varchar(256),
+        "year" int4,
+        duration numeric(18,0),
+        CONSTRAINT songs_pkey PRIMARY KEY (songid)
+    );
+
+    CREATE TABLE public.staging_events (
+        artist varchar(256),
+        auth varchar(256),
+        firstname varchar(256),
+        gender varchar(256),
+        iteminsession int4,
+        lastname varchar(256),
+        length numeric(18,0),
+        "level" varchar(256),
+        location varchar(256),
+        "method" varchar(256),
+        page varchar(256),
+        registration numeric(18,0),
+        sessionid int4,
+        song varchar(256),
+        status int4,
+        ts int8,
+        useragent varchar(256),
+        userid int4
+    );
+
+    CREATE TABLE public.staging_songs (
+        num_songs int4,
+        artist_id varchar(256),
+        artist_name varchar(256),
+        artist_latitude numeric(18,0),
+        artist_longitude numeric(18,0),
+        artist_location varchar(256),
+        song_id varchar(256),
+        title varchar(256),
+        duration numeric(18,0),
+        "year" int4
+    );
+
+    CREATE TABLE public."time" (
+        start_time timestamp NOT NULL,
+        "hour" int4,
+        "day" int4,
+        week int4,
+        "month" varchar(256),
+        "year" int4,
+        weekday varchar(256),
+        CONSTRAINT time_pkey PRIMARY KEY (start_time)
+    );
+
+    CREATE TABLE public.users (
+        userid int4 NOT NULL,
+        first_name varchar(256),
+        last_name varchar(256),
+        gender varchar(256),
+        "level" varchar(256),
+        CONSTRAINT users_pkey PRIMARY KEY (userid)
+    );
+
+    """)
+    songplay_table_insert = ("""
+        SELECT
+                md5(events.sessionid || events.start_time) songplay_id,
+                events.start_time, 
+                events.userid, 
+                events.level, 
+                songs.song_id, 
+                songs.artist_id, 
+                events.sessionid, 
+                events.location, 
+                events.useragent
+                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
+            FROM staging_events
+            WHERE page='NextSong') events
+            LEFT JOIN staging_songs songs
+            ON events.song = songs.title
+                AND events.artist = songs.artist_name
+                AND events.length = songs.duration
+    """)
+
+    user_table_insert = ("""
+        SELECT distinct userid, firstname, lastname, gender, level
+        FROM staging_events
+        WHERE page='NextSong'
+    """)
+
+    song_table_insert = ("""
+        SELECT distinct song_id, title, artist_id, year, duration
+        FROM staging_songs
+    """)
+
+    artist_table_insert = ("""
+        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+        FROM staging_songs
+    """)
+
+    time_table_insert = ("""
+        SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
+               extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
+        FROM songplays
+    """)
